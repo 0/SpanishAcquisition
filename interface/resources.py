596c1214752ba5b48d93dea4ef5b45758b60541e
Dmitri Iouchtchenko <diouchtc@uwaterloo.ca>
2011-05-24 16:38:44 -0400

    Added resources interface

---
+++ interface/resources.py
@@ -0,0 +1,66 @@
+"""
+Tools for working with generic resources.
+"""
+
+
+class NotReadable(Exception):
+	"""
+	Resource cannot be read from.
+	"""
+
+	pass
+
+
+class NotWritable(Exception):
+	"""
+	Resource cannot be written to.
+	"""
+
+	pass
+
+
+class Resource(object):
+	"""
+	A generic resource which can potentially be read from or written to.
+	"""
+
+	def __init__(self, obj=None, getter=None, setter=None):
+		self.obj = obj
+		self.getter = getter
+		self.setter = setter
+
+	@property
+	def value(self):
+		"""
+		The value of the resource.
+		"""
+
+		if self.getter is None:
+			raise NotReadable()
+
+		if callable(self.getter):
+			return self.getter()
+		elif self.obj is not None:
+			return getattr(self.obj, self.getter)
+		else:
+			raise NotReadable()
+
+	@value.setter
+	def value(self, v):
+		if self.setter is None:
+			raise NotWritable()
+
+		if callable(self.setter):
+			self.setter(v)
+		elif self.obj is not None:
+			setattr(self.obj, self.setter, v)
+		else:
+			raise NotWritable()
+
+
+if __name__ == '__main__':
+	import unittest
+
+	from tests import test_resources as my_tests
+
+	unittest.main(module=my_tests)

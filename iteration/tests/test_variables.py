5f903592d603fbb4f0781aa6034d2fc6e087f726
Dmitri Iouchtchenko <diouchtc@uwaterloo.ca>
2011-06-10 10:55:33 -0400

    Added Variable class

---
+++ iteration/tests/test_variables.py
@@ -0,0 +1,81 @@
+from nose.tools import eq_
+import unittest
+
+from iteration import variables
+
+
+class testGroup(unittest.TestCase):
+	def testEmpty(self):
+		"""
+		Use no variables.
+		"""
+
+		iterator, num_items, sorted_variables = variables.combine_variables([])
+		eq_(list(iterator), [])
+		eq_(num_items, 0)
+		eq_(sorted_variables, [])
+
+	def testSingle(self):
+		"""
+		Use a single variable.
+		"""
+
+		var = variables.Variable('Name', 0, -5.0, 5.0, 11, const=6.0)
+
+		expected = [(x,) for x in range(-5, 7)]
+
+		iterator, num_items, sorted_variables = variables.combine_variables([var])
+		eq_(list(iterator), expected)
+		eq_(num_items, len(expected))
+
+	def testMultiple(self):
+		"""
+		Use many variables.
+		"""
+
+		vars = [
+			variables.Variable('A', 1, 1.0, 5.0, 3),
+			variables.Variable('B', 2, 11.0, 12.0, 2, const=10.0),
+			variables.Variable('D', 3, -99.0, 0.0, const=9.0, enabled=False),
+			variables.Variable('C', 2, 21.0, 25.0, 2),
+		]
+
+		expected = [
+			(1.0, 11.0, 21.0, 9.0),
+			(1.0, 12.0, 25.0, 9.0),
+			(3.0, 11.0, 21.0, 9.0),
+			(3.0, 12.0, 25.0, 9.0),
+			(5.0, 11.0, 21.0, 9.0),
+			(5.0, 12.0, 25.0, 9.0),
+			(1.0, 10.0, 21.0, 9.0),
+		]
+
+		iterator, num_items, sorted_variables = variables.combine_variables(vars)
+		eq_(list(iterator), expected)
+		eq_(num_items, len(expected))
+
+		for var, name in zip(sorted_variables, 'ABCD'):
+			eq_(var.name, name)
+
+
+class testVariable(unittest.TestCase):
+	def testIterator(self):
+		"""
+		Create an iterator from a variable.
+		"""
+
+		var = variables.Variable('Name', 1, -1.0, -3.0, 5, const=10.0)
+
+		# Enabled.
+		it1 = var.to_iterator()
+		eq_(list(it1), [-1.0, -1.5, -2.0, -2.5, -3.0])
+
+		# Disabled.
+		var.enabled = False
+
+		it2 = var.to_iterator()
+		eq_(list(it2), [10.0])
+
+
+if __name__ == '__main__':
+	unittest.main()
---
+++ iteration/variables.py
@@ -0,0 +1,116 @@
+import itertools
+import numpy
+import operator
+
+from interface.units import IncompatibleDimensions, SIValues, Quantity
+from iteration.group_iterators import ChainIterator, ParallelIterator, ProductIterator
+
+
+def combine_variables(variables):
+	"""
+	Create a GroupIterator out of some Variables.
+	"""
+
+	if not variables:
+		return ([], 0, [])
+
+	order_attr = operator.attrgetter('order')
+	ordered = sorted(variables, key=order_attr)
+	grouped = ((order, list(vars)) for order, vars in itertools.groupby(ordered, order_attr))
+
+	iterators = []
+	num_items = 1
+	sorted_variables = []
+	# Treat each order to its own parallel iterator.
+	for _, vars in grouped:
+		var_iter = ParallelIterator([x.to_iterator() for x in vars])
+		var_items = sum(1 for _ in var_iter)
+
+		iterators.append(var_iter)
+		num_items *= var_items
+		sorted_variables.extend(vars)
+
+	# This shouldn't be possible, but just in case.
+	if num_items <= 0:
+		return None
+
+	# Just the steps.
+	sweep_iterator = ProductIterator(iterators)
+	# Then include the final values.
+	iterator = ChainIterator([sweep_iterator, [tuple(x.const for x in sorted_variables)]])
+	num_items += 1
+
+	return (iterator, num_items, sorted_variables)
+
+
+class Variable(object):
+	"""
+	A simple linear space variable.
+	"""
+
+	def __init__(self, name, order, initial=0.0, final=0.0, steps=1, enabled=True,
+			wait=0, const=None, resource_name=''):
+		"""
+		name: A string labelling the variable.
+		order: The integer nesting order.
+		"""
+
+		self.name = name
+		self.order = order
+
+		# Linear space parameters.
+		self.initial = initial
+		self.final = final
+		self._steps = steps
+
+		# Iteration parameters.
+		self.enabled = enabled
+		self._wait = Quantity(wait, SIValues.dimensions.time)
+		if const is not None:
+			self.const = const
+		else:
+			self.const = initial
+
+		self.resource_name = resource_name
+
+	@property
+	def steps(self):
+		return self._steps
+
+	@steps.setter
+	def steps(self, value):
+		if value <= 0:
+			raise ValueError('Number of steps must be positive, not "{0}".'.format(value))
+
+		self._steps = value
+
+	@property
+	def wait(self):
+		return str(self._wait)
+
+	@wait.setter
+	def wait(self, value):
+		wait = Quantity.from_string(value)
+
+		if wait.dimension != SIValues.dimensions.time:
+			raise IncompatibleDimensions(wait.dimensions, SIValues.dimensions.time)
+
+		self._wait = wait
+
+	def to_iterator(self):
+		"""
+		Create an iterator for the variable.
+		"""
+
+		if self.enabled:
+			return numpy.linspace(self.initial, self.final, self.steps)
+		else:
+			return [self.const]
+
+
+if __name__ == '__main__':
+	import unittest
+
+	from tests import test_variables as my_tests
+
+	unittest.main(module=my_tests)

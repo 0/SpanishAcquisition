5e0ca98464ce00dd67a5ec98c4ef7b17c292405f
Dmitri Iouchtchenko <diouchtc@uwaterloo.ca>
2011-06-28 12:50:25 -0400

    Added global store and pre-amp settings

---
+++ spacq/gui/global_store.py
@@ -0,0 +1,21 @@
+from threading import RLock
+from pubsub import pub
+
+from spacq.tool.box import PubDict
+
+"""
+A single storage location for everything globally-unique.
+"""
+
+
+class GlobalStore(object):
+	"""
+	A global value store for an entire application.
+	"""
+
+	def __init__(self):
+		self.lock = RLock()
+
+		self.devices = PubDict(self.lock, pub, 'device')
+		self.resources = PubDict(self.lock, pub, 'resource')
+		self.variables = PubDict(self.lock, pub, 'variable')

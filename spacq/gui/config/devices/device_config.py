f9928adaed0dc75011925949f721b77c264a4857
Dmitri Iouchtchenko <diouchtc@uwaterloo.ca>
2011-06-29 11:41:55 -0400

    Added device configuration GUI

---
+++ spacq/gui/config/devices/device_config.py
@@ -0,0 +1,372 @@
+import wx
+from wx.lib.masked.ipaddrctrl import IpAddrCtrl
+
+from spacq.devices.config import device_tree, ConnectionError, DeviceConfig
+
+from ...tool.box import ErrorMessageDialog, load_pickled, save_pickled
+from .resource_tree import DeviceResourcesPanel
+
+"""
+Device configuration for and through the GUI.
+"""
+
+
+class DeviceConfigPanel(wx.Panel):
+	"""
+	Set up a device for consumption of its resources.
+	"""
+
+	def __init__(self, parent, connection_callback=None, *args, **kwargs):
+		wx.Panel.__init__(self, parent, *args, **kwargs)
+
+		self.connection_callback = connection_callback
+
+		# Implementation info.
+		## Find all the available devices.
+		self.device_tree = device_tree()
+		self.manufacturers = [''] + sorted(self.device_tree.keys())
+		self.models = ['']
+
+		## Chosen values.
+		self.manufacturer = None
+		self.model = None
+
+		# Panel.
+		panel_box = wx.BoxSizer(wx.VERTICAL)
+
+		## Address.
+		address_static_box = wx.StaticBox(self, label='Address')
+		address_box = wx.StaticBoxSizer(address_static_box, wx.VERTICAL)
+		address_sizer = wx.BoxSizer(wx.HORIZONTAL)
+		address_box.Add(address_sizer, flag=wx.EXPAND)
+		panel_box.Add(address_box, flag=wx.EXPAND|wx.ALL, border=5)
+
+		### Ethernet.
+		ethernet_static_box = wx.StaticBox(self)
+		ethernet_box = wx.StaticBoxSizer(ethernet_static_box, wx.VERTICAL)
+		address_sizer.Add(ethernet_box, proportion=1)
+
+		self.address_mode_eth = wx.RadioButton(self, label='Ethernet', style=wx.RB_GROUP)
+		ethernet_box.Add(self.address_mode_eth)
+
+		ethernet_sizer = wx.FlexGridSizer(rows=2, cols=2, hgap=5)
+		ethernet_box.Add(ethernet_sizer, flag=wx.EXPAND)
+
+		ethernet_sizer.Add(wx.StaticText(self, label='IP address:'),
+				flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT)
+		self.ip_address_input = IpAddrCtrl(self)
+		ethernet_sizer.Add(self.ip_address_input, flag=wx.CENTER)
+
+		### GPIB.
+		self.gpib_static_box = wx.StaticBox(self)
+		gpib_box = wx.StaticBoxSizer(self.gpib_static_box, wx.VERTICAL)
+		address_sizer.Add(gpib_box, proportion=1)
+
+		self.address_mode_gpib = wx.RadioButton(self, label='GPIB')
+		gpib_box.Add(self.address_mode_gpib)
+
+		gpib_sizer = wx.FlexGridSizer(rows=3, cols=2, hgap=5)
+		gpib_box.Add(gpib_sizer, flag=wx.EXPAND)
+
+		gpib_sizer.Add(wx.StaticText(self, label='Board:'),
+				flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT)
+		self.gpib_board_input = wx.SpinCtrl(self, min=0, max=100, initial=0)
+		gpib_sizer.Add(self.gpib_board_input, flag=wx.CENTER)
+
+		gpib_sizer.Add(wx.StaticText(self, label='PAD:'),
+				flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT)
+		self.gpib_pad_input = wx.SpinCtrl(self, min=1, max=30, initial=1)
+		gpib_sizer.Add(self.gpib_pad_input, flag=wx.CENTER)
+
+		gpib_sizer.Add(wx.StaticText(self, label='SAD:'),
+				flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT)
+		self.gpib_sad_input = wx.SpinCtrl(self, min=0, max=30, initial=0)
+		gpib_sizer.Add(self.gpib_sad_input, flag=wx.CENTER)
+
+		### USB.
+		usb_static_box = wx.StaticBox(self)
+		usb_box = wx.StaticBoxSizer(usb_static_box, wx.VERTICAL)
+		address_box.Add(usb_box, flag=wx.EXPAND)
+
+		self.address_mode_usb = wx.RadioButton(self, label='USB')
+		usb_box.Add(self.address_mode_usb)
+
+		usb_sizer = wx.BoxSizer(wx.HORIZONTAL)
+		usb_box.Add(usb_sizer, flag=wx.EXPAND)
+
+		usb_sizer.Add(wx.StaticText(self, label='USB resource: '),
+				flag=wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_RIGHT)
+		self.usb_resource_input = wx.TextCtrl(self, size=(300, -1))
+		usb_sizer.Add(self.usb_resource_input, proportion=1)
+
+		## Implementation.
+		implementation_static_box = wx.StaticBox(self, label='Implementation')
+		implementation_box = wx.StaticBoxSizer(implementation_static_box, wx.HORIZONTAL)
+		panel_box.Add(implementation_box, flag=wx.EXPAND|wx.ALL, border=5)
+
+		self.manufacturer_input = wx.Choice(self, choices=self.manufacturers)
+		self.Bind(wx.EVT_CHOICE, self.OnManufacturer, self.manufacturer_input)
+		implementation_box.Add(self.manufacturer_input, proportion=1)
+
+		self.model_input = wx.Choice(self, choices=self.models)
+		self.Bind(wx.EVT_CHOICE, self.OnModel, self.model_input)
+		implementation_box.Add(self.model_input, proportion=1)
+
+		self.mock_input = wx.CheckBox(self, label='Mock')
+		implementation_box.Add(self.mock_input, flag=wx.CENTER)
+
+		## Connection buttons.
+		button_box = wx.BoxSizer(wx.HORIZONTAL)
+		panel_box.Add(button_box, flag=wx.CENTER|wx.ALL, border=5)
+
+		self.connect_button = wx.Button(self, label='Connect')
+		self.Bind(wx.EVT_BUTTON, self.OnConnect, self.connect_button)
+		button_box.Add(self.connect_button)
+
+		self.disconnect_button = wx.Button(self, label='Disconnect')
+		self.Bind(wx.EVT_BUTTON, self.OnDisconnect, self.disconnect_button)
+		button_box.Add(self.disconnect_button)
+
+		self.SetSizerAndFit(panel_box)
+
+	def get_address_mode(self):
+		if self.address_mode_eth.Value:
+			return DeviceConfig.address_modes.ethernet
+		elif self.address_mode_gpib.Value:
+			return DeviceConfig.address_modes.gpib
+		elif self.address_mode_usb.Value:
+			return DeviceConfig.address_modes.usb
+
+	def GetValue(self):
+		dev_cfg = DeviceConfig()
+
+		# Address mode.
+		dev_cfg.address_mode = self.get_address_mode()
+
+		## Ethernet.
+		possible_address = self.ip_address_input.GetAddress()
+		if self.ip_address_input.IsValid() and len(possible_address) > 6:
+			dev_cfg.ip_address = possible_address
+		else:
+			dev_cfg.ip_address = None
+
+		## GPIB.
+		dev_cfg.gpib_board = self.gpib_board_input.Value
+		dev_cfg.gpib_pad = self.gpib_pad_input.Value
+		dev_cfg.gpib_sad = self.gpib_sad_input.Value
+
+		## USB.
+		possible_resource = self.usb_resource_input.Value
+		if possible_resource:
+			dev_cfg.usb_resource = possible_resource
+		else:
+			dev_cfg.usb_resource = None
+
+		# Implementation.
+		dev_cfg.manufacturer = self.manufacturer
+		dev_cfg.model = self.model
+		dev_cfg.mock = self.mock_input.Value
+
+		# Device.
+		dev_cfg.device = self.device
+
+		# Resource labels.
+		dev_cfg.resource_labels = self.resource_labels
+
+		return dev_cfg
+
+	def SetValue(self, dev_cfg):
+		# Address mode.
+		if dev_cfg.address_mode == DeviceConfig.address_modes.ethernet:
+			self.address_mode_eth.Value = True
+		elif dev_cfg.address_mode == DeviceConfig.address_modes.gpib:
+			self.address_mode_gpib.Value = True
+		elif dev_cfg.address_mode == DeviceConfig.address_modes.usb:
+			self.address_mode_usb.Value = True
+
+		## Ethernet.
+		if dev_cfg.ip_address:
+			self.ip_address_input.SetValue(dev_cfg.ip_address)
+
+		## GPIB.
+		self.gpib_board_input.Value = dev_cfg.gpib_board
+		self.gpib_pad_input.Value = dev_cfg.gpib_pad
+		self.gpib_sad_input.Value = dev_cfg.gpib_sad
+
+		## USB.
+		if dev_cfg.usb_resource:
+			self.usb_resource_input.Value = dev_cfg.usb_resource
+
+		# Implementation.
+		if dev_cfg.manufacturer is not None:
+			self.manufacturer_input.StringSelection = dev_cfg.manufacturer
+			self.OnManufacturer()
+
+		if dev_cfg.model is not None:
+			self.model_input.StringSelection = dev_cfg.model
+			self.OnModel()
+
+		self.mock_input.Value = dev_cfg.mock
+
+		# Device.
+		self.device = dev_cfg.device
+		if self.device is not None:
+			self.connect_button.Disable()
+			self.disconnect_button.Enable()
+		else:
+			self.connect_button.Enable()
+			self.disconnect_button.Disable()
+
+		# Resource labels.
+		self.resource_labels = dev_cfg.resource_labels
+
+		if self.device is not None and self.connection_callback is not None:
+			self.connection_callback(self.device, self.resource_labels)
+
+	def OnManufacturer(self, evt=None):
+		self.manufacturer = self.manufacturers[self.manufacturer_input.Selection]
+
+		self.models = ['']
+		if self.manufacturer:
+			self.models.extend(self.device_tree[self.manufacturer].keys())
+		else:
+			self.manufacturer = None
+
+		self.model_input.SetItems(self.models)
+
+	def OnModel(self, evt=None):
+		self.model = self.models[self.model_input.Selection]
+
+		if self.model:
+			model = self.device_tree[self.manufacturer][self.model]
+
+			if 'real' in model and 'mock' not in model:
+				self.mock_input.Value = False
+				self.mock_input.Disable()
+			elif 'real' not in model and 'mock' in model:
+				self.mock_input.Value = True
+				self.mock_input.Disable()
+			else:
+				self.mock_input.Enable()
+		else:
+			self.model = None
+
+	def OnDisconnect(self, evt=None):
+		self.device = None
+
+		if self.connection_callback is not None:
+			self.connection_callback(None, {})
+
+		self.disconnect_button.Disable()
+
+	def OnConnect(self, evt=None):
+		dev_cfg = self.GetValue()
+		try:
+			dev_cfg.connect()
+		except ConnectionError as e:
+			ErrorMessageDialog(self, str(e), 'Connection error').Show()
+			return
+
+		self.device = dev_cfg.device
+
+		if self.connection_callback is not None:
+			self.connection_callback(self.device, self.resource_labels)
+
+		self.connect_button.Disable()
+
+
+class DeviceConfigDialog(wx.Dialog):
+	"""
+	A dialog for configuring a device, including connection and resources.
+	"""
+
+	def __init__(self, parent, ok_callback, *args, **kwargs):
+		wx.Dialog.__init__(self, parent, style=wx.DEFAULT_DIALOG_STYLE|wx.RESIZE_BORDER,
+				*args, **kwargs)
+
+		self.ok_callback = ok_callback
+
+		# Dialog.
+		dialog_box = wx.BoxSizer(wx.VERTICAL)
+
+		## Tabs.
+		self.notebook = wx.Notebook(self)
+		dialog_box.Add(self.notebook, proportion=1, flag=wx.EXPAND)
+
+		self.resources_panel = DeviceResourcesPanel(self.notebook)
+		self.connection_panel = DeviceConfigPanel(self.notebook,
+				connection_callback=self.resources_panel.set_device)
+
+		self.notebook.AddPage(self.connection_panel, 'Connection')
+		self.notebook.AddPage(self.resources_panel, 'Resources')
+
+		## End buttons.
+		button_box = wx.BoxSizer(wx.HORIZONTAL)
+		dialog_box.Add(button_box, flag=wx.CENTER|wx.TOP, border=10)
+
+		### OK, cancel.
+		dialog_button_box = wx.BoxSizer(wx.HORIZONTAL)
+		button_box.Add(dialog_button_box)
+
+		ok_button = wx.Button(self, wx.ID_OK)
+		self.Bind(wx.EVT_BUTTON, self.OnOk, ok_button)
+		dialog_button_box.Add(ok_button)
+
+		cancel_button = wx.Button(self, wx.ID_CANCEL)
+		dialog_button_box.Add(cancel_button)
+
+		### Save, load.
+		save_button_box = wx.BoxSizer(wx.HORIZONTAL)
+		button_box.Add(save_button_box, flag=wx.LEFT, border=20)
+
+		save_button = wx.Button(self, wx.ID_SAVE, label='Save...')
+		self.Bind(wx.EVT_BUTTON, self.OnSave, save_button)
+		save_button_box.Add(save_button)
+
+		load_button = wx.Button(self, wx.ID_OPEN, label='Load...')
+		self.Bind(wx.EVT_BUTTON, self.OnLoad, load_button)
+		save_button_box.Add(load_button)
+
+		self.SetSizerAndFit(dialog_box)
+
+	def GetValue(self):
+		dev_cfg = self.connection_panel.GetValue()
+		labels, resources = self.resources_panel.GetValue()
+
+		dev_cfg.resources = resources
+
+		# Preserve labels between device instances.
+		if dev_cfg.device is not None:
+			dev_cfg.resource_labels = labels
+
+		return dev_cfg
+
+	def SetValue(self, dev_cfg):
+		self.connection_panel.SetValue(dev_cfg)
+		self.resources_panel.SetValue(dev_cfg.resource_labels, dev_cfg.resources)
+
+	def OnOk(self, evt=None):
+		self.ok_callback(self)
+
+		self.Destroy()
+
+	def OnSave(self, evt=None):
+		try:
+			save_pickled(self, self.GetValue(), extension='dev',
+					file_type='Device configuration')
+		except IOError as e:
+			ErrorMessageDialog(self, str(e), 'Save error').Show()
+			return
+
+	def OnLoad(self, evt=None):
+		try:
+			value = load_pickled(self, extension='dev', file_type='Device configuration')
+
+			try:
+				if value is not None:
+					self.SetValue(value)
+			except Exception as e:
+				raise IOError('Could not set values.', e)
+		except IOError as e:
+			ErrorMessageDialog(self, str(e), 'Load error').Show()
+			return
---
+++ spacq/gui/config/devices/resource_tree.py
@@ -0,0 +1,314 @@
+from threading import Thread
+import wx
+from wx.gizmos import TreeListCtrl
+
+from spacq.interface.resources import NotReadable
+
+from ...tool.box import ErrorMessageDialog
+
+"""
+A tree of subdevices and resources.
+"""
+
+
+class ResourceFetcher(Thread):
+	"""
+	A thread which iterates over a list of items, getting resource values.
+	"""
+
+	def __init__(self, items, getter, callback, *args, **kwargs):
+		"""
+		items: List of TreeListCtrl items.
+		getter: Given an item, returns a Resource or None.
+		callback: Is called with the item and the value of the resource.
+		"""
+
+		Thread.__init__(self, *args, **kwargs)
+
+		self.items = items
+		self.getter = getter
+		self.callback = callback
+
+	def run(self):
+		for item in self.items:
+			resource = self.getter(item)
+
+			if resource is not None and resource.readable:
+				wx.CallAfter(self.callback, item, resource.value)
+
+
+class ItemData(object):
+	"""
+	Useful information about a node to be stored in its PyData.
+	"""
+
+	def __init__(self, path, resource):
+		self.path = path
+		self.resource = resource
+
+		self.fetched = False
+
+
+class ResourceTree(TreeListCtrl):
+	"""
+	A tree list to display an hierarchy of subdevices and resources.
+	"""
+
+	def __init__(self, parent, *args, **kwargs):
+		TreeListCtrl.__init__(self, parent, *args,
+				style=wx.TR_DEFAULT_STYLE|wx.TR_FULL_ROW_HIGHLIGHT|wx.TR_HIDE_ROOT,
+				**kwargs)
+
+		self.root = None
+
+		self.col_name = 0
+		self.AddColumn('Name', 200)
+		self.col_r = 1
+		self.AddColumn('R', 24)
+		self.col_w = 2
+		self.AddColumn('W', 24)
+		self.col_label = 3
+		self.AddColumn('Label', 200, edit=True)
+		self.col_value = 4
+		self.AddColumn('Value', 400, edit=True)
+
+		self.SetMinSize((850, -1))
+
+		self.Bind(wx.EVT_TREE_ITEM_EXPANDED, self.OnItemExpanded)
+		self.Bind(wx.EVT_TREE_BEGIN_LABEL_EDIT, self.OnBeginLabelEdit)
+		self.Bind(wx.EVT_TREE_END_LABEL_EDIT, self.OnEndLabelEdit)
+		self.Bind(wx.EVT_TREE_ITEM_ACTIVATED, self.OnActivated)
+
+	def GetChildren(self, item):
+		"""
+		Non-recursive generator for the children of an item.
+		"""
+
+		if self.HasChildren(item):
+			child, cookie = self.GetFirstChild(item)
+
+			while child:
+				yield child
+
+				child, cookie = self.GetNextChild(item, cookie)
+
+	def GetLeaves(self, item=None):
+		"""
+		Recursively collect the leaves under an item.
+		"""
+
+		if item is None:
+			if self.root is None:
+				return []
+			else:
+				item = self.root
+
+		if not self.HasChildren(item):
+			return [item]
+		else:
+			result = []
+
+			for child in self.GetChildren(item):
+				result.extend(self.GetLeaves(child))
+
+			return result
+
+	def fell(self):
+		"""
+		Cut down the tree.
+		"""
+
+		self.DeleteAllItems()
+		self.root = None
+
+	def spawn_fetch_thread(self, items):
+		"""
+		Create a thread to populate the items.
+		"""
+
+		def fetch(item):
+			pydata = self.GetItemPyData(item)
+
+			if pydata is not None:
+				if not pydata.fetched:
+					pydata.fetched = True
+
+					return pydata.resource
+
+		def set(item, value):
+			self.SetItemText(item, str(value), self.col_value)
+
+		thr = ResourceFetcher(items, fetch, set)
+		thr.daemon = True
+		thr.start()
+
+	def build_tree(self, device, resource_labels, root=None, path=None):
+		"""
+		Recursively append all subdevices and resources.
+		"""
+
+		if root is None:
+			self.fell()
+
+			self.root = self.AddRoot('')
+			root = self.root
+
+			path = ()
+
+		for name, subdev in device.subdevices.items():
+			item = self.AppendItem(root, name)
+			full_path = path + (name,)
+
+			self.build_tree(subdev, resource_labels, item, full_path)
+
+		for name, resource in device.resources.items():
+			item = self.AppendItem(root, name)
+			full_path = path + (name,)
+
+			if resource.getter is not None:
+				self.SetItemText(item, 'R', self.col_r)
+
+			if resource.setter is not None:
+				self.SetItemText(item, 'W', self.col_w)
+
+			self.SetItemPyData(item, ItemData(full_path, resource))
+
+			if full_path in resource_labels:
+				self.SetItemText(item, resource_labels[full_path], self.col_label)
+
+		self.SortChildren(root)
+
+		if root == self.root:
+			self.spawn_fetch_thread(self.GetChildren(self.root))
+
+	def set_value(self, item, value):
+		"""
+		Set the value of a resource, as well as the displayed value.
+		"""
+
+		pydata = self.GetItemPyData(item)
+		resource = pydata.resource
+
+		def update():
+			resource.value = resource.convert(value)
+
+			try:
+				true_value = str(resource.value)
+			except NotReadable:
+				pass
+			else:
+				wx.CallAfter(self.SetItemText, item, true_value, self.col_value)
+
+		thr = Thread(target=update)
+		thr.daemon = True
+		thr.start()
+
+	def OnItemExpanded(self, evt):
+		"""
+		Get any resources which may now be visible.
+		"""
+
+		self.spawn_fetch_thread(self.GetChildren(evt.Item))
+
+	def OnBeginLabelEdit(self, evt):
+		# EVT_TREE_END_LABEL_EDIT does not carry this value.
+		self.editing_col = evt.Int
+
+		if evt.Int == self.col_label:
+			# Only resources can have labels.
+			if not (self.GetItemText(evt.Item, self.col_r) or
+					self.GetItemText(evt.Item, self.col_w)):
+				evt.Veto()
+		elif evt.Int == self.col_value:
+			# Can only write to writable resources.
+			if not self.GetItemText(evt.Item, self.col_w):
+				evt.Veto()
+
+			pydata = self.GetItemPyData(evt.Item)
+			resource = pydata.resource
+
+			if resource.allowed_values is not None:
+				options = [str(x) for x in sorted(resource.allowed_values)]
+
+				dlg = wx.SingleChoiceDialog(self, '', 'Choose value', options)
+				# Select the current value if possible.
+				try:
+					dlg.SetSelection(options.index(self.GetItemText(evt.Item, self.col_value)))
+				except ValueError:
+					pass
+
+				if dlg.ShowModal() == wx.ID_OK:
+					try:
+						self.set_value(evt.Item, dlg.GetStringSelection())
+					except ValueError as e:
+						ErrorMessageDialog(self, str(e), 'Invalid value').Show()
+						return
+
+				# No need for the editor.
+				evt.Veto()
+		else:
+			evt.Veto()
+
+	def OnEndLabelEdit(self, evt):
+		if self.editing_col == self.col_value:
+			# Update the real value.
+			value = evt.Label
+
+			try:
+				self.set_value(evt.Item, value)
+			except ValueError as e:
+				ErrorMessageDialog(self, str(e), 'Invalid value').Show()
+				return
+
+	def OnActivated(self, evt):
+		"""
+		Double click to edit.
+		"""
+
+		self.EditLabel(evt.Item, evt.Int)
+
+
+class DeviceResourcesPanel(wx.Panel):
+	"""
+	A panel for displaying the subdevices and resources of a device.
+	"""
+
+	def __init__(self, parent, *args, **kwargs):
+		wx.Panel.__init__(self, parent, *args, **kwargs)
+
+		# Panel.
+		panel_box = wx.BoxSizer(wx.VERTICAL)
+
+		## Tree.
+		self.tree = ResourceTree(self)
+		panel_box.Add(self.tree, proportion=1, flag=wx.EXPAND)
+
+		self.SetSizer(panel_box)
+
+	def set_device(self, device, resource_labels):
+		if device is None:
+			self.tree.fell()
+		else:
+			self.tree.build_tree(device, resource_labels)
+
+	def GetValue(self):
+		labels = {}
+		resources = {}
+
+		for leaf in self.tree.GetLeaves():
+			pydata = self.tree.GetItemPyData(leaf)
+			name = self.tree.GetItemText(leaf, self.tree.col_label)
+
+			if name:
+				labels[pydata.path] = name
+				resources[name] = pydata.resource
+
+		return (labels, resources)
+
+	def SetValue(self, resource_labels, resources):
+		for path, name in resource_labels.items():
+			for leaf in self.tree.GetLeaves():
+				pydata = self.tree.GetItemPyData(leaf)
+
+				if pydata.path == path:
+					self.tree.SetItemText(leaf, name, self.tree.col_label)

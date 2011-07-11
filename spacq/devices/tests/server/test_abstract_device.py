e02fc5bb16359c6aa867283d8bca7708aec4493d
Dmitri Iouchtchenko <diouchtc@uwaterloo.ca>
2011-07-11 12:08:05 -0400

    Made AbstractDeviceTest more generic

--- spacq/devices/tests/server/test_abstract_device.py
+++ spacq/devices/tests/server/test_abstract_device.py
@@ -52,16 +52,15 @@ class AbstractDeviceTest(unittest.TestCase):
 			except:
 				continue
 
-			# Some values to check against.
+			# Value to check against.
 			id = dev.idn
-			ver = dev.ask('system:version?')
 
-			expected = [ver, ver, id]
+			expected = ['1'] * 3 + [id]
 
 			dev.multi_command_start()
-			dev.ask('system:version?')
 			dev.opc
-			dev.ask('system:version?')
+			dev.opc
+			dev.opc
 			dev.idn
 			responses = dev.multi_command_stop()
 

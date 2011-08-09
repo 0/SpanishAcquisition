54cd126771dcc0017825569cc295a6e82343c7ef
Dmitri Iouchtchenko <diouchtc@uwaterloo.ca>
2011-08-09 14:21:32 -0400

    Fixed DeviceConfig.connect to not complain needlessly
    Mock devices don't have addresses.

--- spacq/devices/config.py
+++ spacq/devices/config.py
@@ -173,20 +173,21 @@ class DeviceConfig(object):
 
 		address = {}
 
-		if self.address_mode == self.address_modes.ethernet:
-			if self.ip_address is None:
-				raise ConnectionError('No IP address specified.')
+		if not self.mock:
+			if self.address_mode == self.address_modes.ethernet:
+				if self.ip_address is None:
+					raise ConnectionError('No IP address specified.')
 
-			address['ip_address'] = self.ip_address
-		elif self.address_mode == self.address_modes.gpib:
-			address['gpib_board'] = self.gpib_board
-			address['gpib_pad'] = self.gpib_pad
-			address['gpib_sad'] = self.gpib_sad
-		elif self.address_mode == self.address_modes.usb:
-			if self.usb_resource is None:
-				raise ConnectionError('No USB resource specified.')
+				address['ip_address'] = self.ip_address
+			elif self.address_mode == self.address_modes.gpib:
+				address['gpib_board'] = self.gpib_board
+				address['gpib_pad'] = self.gpib_pad
+				address['gpib_sad'] = self.gpib_sad
+			elif self.address_mode == self.address_modes.usb:
+				if self.usb_resource is None:
+					raise ConnectionError('No USB resource specified.')
 
-			address['usb_resource'] = self.usb_resource
+				address['usb_resource'] = self.usb_resource
 
 		tree = device_tree()
 

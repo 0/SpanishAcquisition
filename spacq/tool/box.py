5fc885fd319a56e6d8870ca475d4818a78bcea31
Dmitri Iouchtchenko <diouchtc@uwaterloo.ca>
2011-08-19 14:07:16 -0400

    Moved triples_to_mesh to spacq.tool.box
    It didn't really belong in spacq.iteration at all.

--- spacq/tool/box.py
+++ spacq/tool/box.py
@@ -1,5 +1,7 @@
 from functools import wraps
 from itertools import chain
+from numpy import linspace, meshgrid, sort, unique
+from scipy.interpolate import griddata
 
 """
 Generic tools.
@@ -20,6 +22,30 @@ def sift(items, cls):
 	"""
 
 	return [item for item in items if isinstance(item, cls)]
+
+
+def triples_to_mesh(x, y, z):
+	"""
+	Convert 3 equal-sized lists of co-ordinates into an interpolated 2D mesh of z-values.
+
+	Returns a tuple of:
+		the mesh
+		the x bounds
+		the y bounds
+		the z bounds
+	"""
+
+	x_values, y_values = sort(unique(x)), sort(unique(y))
+
+	x_space = linspace(x_values[0], x_values[-1], len(x_values))
+	y_space = linspace(y_values[0], y_values[-1], len(y_values))
+
+	target_x, target_y = meshgrid(x_space, y_space)
+
+	target_z = griddata((x, y), z, (target_x, target_y), method='cubic')
+
+	return (target_z, (x_values[0], x_values[-1]), (y_values[0], y_values[-1]),
+			(min(z), max(z)))
 
 
 class Enum(set):
